84437db7c95b2f5250bd33a13fe58ed01de464b3
emilutz <emil.barbutza@gmail.com>
2018-03-01 18:16:43 +0000

    eirini

--- script.py
+++ script.py
@@ -1,10 +1,38 @@
+import sys
 import glob
+import numpy as np
 
 
-def main():
+def read_data(path):
+	with open(path, 'r') as f:
+		for i, row in enumerate(f):
+
+			# read first line
+			if i == 0:
+				R, C, F, N, B, T = map(lambda x: int(x), row.split(' '))
+				print(C)
+				break
+
+
+
+def run_all():
 
 	input_files = glob.glob("inputs/*.in")
+	for input_file in input_files:
+		read_data(input_file)
+
+
+def run_single(file_path):
+	read_data(file_path)
+
 
 
 if __name__ == "__main__":
-	main()
+
+	if len(sys.argv) < 2:
+		run_all()
+	if len(sys.argv) == 2:
+		file_path = sys.argv[1]
+		run_single(file_path)
+	else:
+		sys.exit('Wrong input !')
