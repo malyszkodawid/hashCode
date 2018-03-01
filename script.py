ce1508f3e70dc526aaa9587bf6150783439d63d4
Dawid <dawid@dawid.pl>
2018-03-01 18:18:17 +0000

    solve function

--- script.py
+++ script.py
@@ -4,7 +4,9 @@ import numpy as np
 
 
 def read_data(path):
+
 	with open(path, 'r') as f:
+		
 		for i, row in enumerate(f):
 
 			# read first line
@@ -14,16 +16,21 @@ def read_data(path):
 				break
 
 
+def solve(path):
+
+	data = read_data(path)
+
+
 
 def run_all():
 
 	input_files = glob.glob("inputs/*.in")
 	for input_file in input_files:
-		read_data(input_file)
+		solve(input_file)
 
 
 def run_single(file_path):
-	read_data(file_path)
+	solve(file_path)
 
 
 
